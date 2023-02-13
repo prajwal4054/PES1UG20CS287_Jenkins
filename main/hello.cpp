a5452b9013a5284394a97ef87ead2225ad580dcb
prajwal4054 <prajwal4054@gmail.com>
2023-02-13 15:05:51 +0530

    Webhook commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
+  cout<<"Hello automatically executed using webhooks\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
